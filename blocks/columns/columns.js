cfbc3921f7bbf558840a7035dbe7218a2607c5da
Daniel <daniel.cini@thepixel.com>
2024-07-08 12:57:48 +0200

    added js onhover for balenciaga button + styling

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -5,14 +5,27 @@ export default function decorate(block) {
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
+      const pic = col.querySelector("picture");
       if (pic) {
-        const picWrapper = pic.closest('div');
+        const picWrapper = pic.closest("div");
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
+          picWrapper.classList.add("columns-img-col");
         }
       }
     });
+  });
+
+  // SHOP NOW > onhover animation
+  const button = document.querySelector(
+    "  .columns-container:nth-child(4)> .columns-wrapper> .columns-2-cols div:nth-child(2) div:nth-child(1) p:nth-child(2)"
+  );
+
+  button.addEventListener("mouseover", () => {
+    button.style.opacity = 0.5;
+  });
+
+  button.addEventListener("mouseout", () => {
+    button.style.opacity = 1;
   });
 }
