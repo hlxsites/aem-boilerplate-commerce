5cca774a74b72f51b16fdabb6fb8dd6561910ce2
christopherSpiteri-Pixel <christopher.spiteri@thepixel.com>
2024-07-23 11:46:00 +0200

    changes in responsiveness and image sizing

---
+++ blocks/hero/hero.js
@@ -0,0 +1,17 @@
+export default function decorate(block) 
+{
+  
+    //Christopher - Search
+    const searchIconSecion = document.querySelector(
+      `.hero > div > div > :has(span)`
+    );
+
+    const inputField = document.createElement("input");
+    inputField.setAttribute("type","text");
+    inputField.setAttribute("id","search-input");
+    inputField.setAttribute("size","1000");
+    inputField.setAttribute("maxlength","1000");
+    inputField.setAttribute("placeholder","Search articles");
+    
+    searchIconSecion.appendChild(inputField)
+  }
