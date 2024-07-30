321d59cb6481b03fc8e83406774e6d4bca212c60
christopherSpiteri-Pixel <christopher.spiteri@thepixel.com>
2024-07-30 12:07:09 +0200

    added modal function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -92,6 +92,20 @@ async function loadFonts() {
   }
 }
 
+// modal function
+
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
+
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
@@ -334,3 +348,4 @@ async function loadPage() {
 }
 
 loadPage();
+
