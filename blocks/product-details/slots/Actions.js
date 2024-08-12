9fb77341a0396ad084f8ce74341449044b7b7547
christopherSpiteri-Pixel <christopher.spiteri@thepixel.com>
2024-08-12 17:19:58 +0200

    updated Action.js to make delivery estimate dynamic

--- blocks/product-details/slots/Actions.js
+++ blocks/product-details/slots/Actions.js
@@ -1,11 +1,13 @@
+import { events } from '@dropins/tools/event-bus.js';
+
 export default function Actions(ctx)
 {
     const SpecialMessage = document.createElement('div');
     SpecialMessage.classList.add('SpecialMessageText');
-    SpecialMessage.innerHTML = `<span data-test-id="ShippingMessage__OrderText">Order within 1 Hours 6 Minutes</span> 
-                                to receive <em data-test-id="ShippingMessage__DeliveryEstimate">Mon 12 Aug</em>`;
+    SpecialMessage.innerHTML = `<span data-test-id="ShippingMessage__OrderText"></span> 
+                                to receive <em data-test-id="ShippingMessage__DeliveryEstimate"></em>`;
 
-    ctx.replaceWith(SpecialMessage);
+    ctx.appendChild(SpecialMessage);
 
     ctx.appendButton(() => {
         return{
@@ -21,4 +23,45 @@ export default function Actions(ctx)
     });
     
     // TODO: make counter of shipping message dynamic
+    events.on('eds/lcp', () => {
+        const deadline = new Date();
+        
+        deadline.setHours(17);
+
+        const x = setInterval(function(){
+            var now = new Date().getTime();
+
+            var distance = deadline - now;
+            // var distance = 0;
+
+            var hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+            var minutes = Math.floor((distance % (1000*60*60))/(1000*60));
+            
+            if(hours > 0)
+            {
+                document.querySelector(`[data-test-id='ShippingMessage__OrderText']`).innerHTML = "Order within " + hours + " hours " + minutes + " mins";
+            }        
+            else
+            {
+                document.querySelector(`[data-test-id='ShippingMessage__OrderText']`).innerHTML = "Order within " + minutes + " mins";
+            }
+
+            if(distance <= 0)
+            {
+                document.querySelector(`[data-test-id='ShippingMessage__OrderText']`).innerHTML = "Order Now";
+            }
+
+        });
+
+        const weekday = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
+        const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+
+        const day = weekday[tomorrow.getDay()];
+        const month = months[tomorrow.getMonth()];
+
+        document.querySelector(`[data-test-id='ShippingMessage__DeliveryEstimate']`).innerHTML = day + " " + tomorrow.getDate() + " " + month;
+    });
 }
