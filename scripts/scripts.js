8e798301b73a4cb24cefa1586a1504e78766b06f
Daniel <daniel.cini@thepixel.com>
2024-08-28 10:39:03 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -291,15 +291,16 @@ async function loadLazy(doc) {
 
   const main = doc.querySelector('main');
   const headerPromise = loadHeader(doc.querySelector('header'));
+
   await loadBlocks(main);
-  await headerPromise;
 
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
+  await headerPromise;
   await Promise.all([
-    loadHeader(doc.querySelector('header')),
+    // loadHeader(doc.querySelector('header')),
     loadFooter(doc.querySelector('footer')),
     loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`),
     loadFonts(),
