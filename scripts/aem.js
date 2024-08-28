53dbc52e169774029d379cacaf65b42483165e6b
Daniel <daniel.cini@thepixel.com>
2024-08-28 09:14:46 +0200

    .

--- scripts/aem.js
+++ scripts/aem.js
@@ -702,6 +702,18 @@ async function loadBlocks(main) {
 }
 
 /**
+ * Loads a block named 'header' into header
+ * @param {Element} header header element
+ * @returns {Promise}
+ */
+async function loadHeader(header) {
+  const headerBlock = buildBlock('header', '');
+  header.append(headerBlock);
+  decorateBlock(headerBlock);
+  return loadBlock(headerBlock);
+}
+
+/**
  * Decorates a block.
  * @param {Element} block The block element
  */
@@ -725,18 +737,6 @@ function decorateBlock(block) {
  */
 function decorateBlocks(main) {
   main.querySelectorAll('div.section > div > div').forEach(decorateBlock);
-}
-
-/**
- * Loads a block named 'header' into header
- * @param {Element} header header element
- * @returns {Promise}
- */
-async function loadHeader(header) {
-  const headerBlock = buildBlock('header', '');
-  header.append(headerBlock);
-  decorateBlock(headerBlock);
-  return loadBlock(headerBlock);
 }
 
 /**
