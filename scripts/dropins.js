c9541fa1ac32515efef80464a81f06fb6f94740b
Abrasimov Yaroslav <y.abrasimov@atwix.com>
2024-09-17 14:05:00 +0200

    Move order initializer logic to dropins.js

--- scripts/dropins.js
+++ scripts/dropins.js
@@ -12,12 +12,14 @@ import { initializers, Initializer } from '@dropins/tools/initializer.js';
 // Drop-ins
 import * as authApi from '@dropins/storefront-auth/api.js';
 import * as cartApi from '@dropins/storefront-cart/api.js';
+import * as orderApi from '@dropins/storefront-order/api.js';
 
 // Recaptcha
 import * as recaptcha from '@dropins/tools/recaptcha.js';
 
 // Libs
 import { getConfigValue, getCookie } from './configs.js';
+import { getMetadata } from './aem.js';
 
 export const getUserTokenCookie = () => getCookie('auth_dropin_user_token');
 
@@ -61,6 +63,69 @@ export default async function initializeDropins() {
   initializers.register(initialize, {});
   initializers.register(authApi.initialize, {});
   initializers.register(cartApi.initialize, {});
+
+  // Get current page template metadata
+  const templateMeta = getMetadata('template');
+  const isOrderPage = templateMeta.includes('Order');
+
+  if (isOrderPage) {
+    const isAuthenticated = !!getCookie('auth_dropin_user_token');
+
+    const currentUrl = new URL(window.location.href);
+    const orderRef = currentUrl.searchParams.get('orderRef');
+    const orderToken = orderRef && orderRef.length > 20 ? orderRef : null;
+    const orderNumber = orderRef && orderRef.length < 20 ? orderRef : null;
+
+    if (!isAuthenticated) {
+      if (orderToken) {
+        window.location.href = `/order-details?orderRef=${orderToken}`;
+      } else if (orderNumber) {
+        window.location.href = `/order-status?orderRef=${orderNumber}`;
+      } else {
+        window.location.href = '/order-status';
+      }
+    } else {
+      events.on('order/error', () => {
+        const isAuthenticatedOnErrorEvent = !!getCookie('auth_dropin_user_token');
+
+        if (isAuthenticatedOnErrorEvent) {
+          window.location.href = '/customer/orders';
+        } else {
+          window.location.href = '/order-status';
+        }
+      });
+
+      events.on('order/data', (orderData) => {
+        if (!orderData) {
+          const isAuthenticatedOnDataEvent = !!getCookie('auth_dropin_user_token');
+
+          if (isAuthenticatedOnDataEvent) {
+            window.location.href = '/customer/orders';
+          } else {
+            window.location.href = '/order-status';
+          }
+        }
+      });
+
+      if (orderNumber && !isAuthenticated) {
+        window.location.href = `/order-status?orderRef=${orderNumber}`;
+      } else if (orderNumber && isAuthenticated) {
+        if (!window.location.href.includes(`/customer/order-details?orderRef=${orderNumber}`)) {
+          window.location.href = `/customer/order-details?orderRef=${orderNumber}`;
+        } else {
+          initializers.register(orderApi.initialize, {
+            orderRef: orderNumber,
+          });
+        }
+      }
+
+      if (orderToken) {
+        initializers.register(orderApi.initialize, {
+          orderRef: orderToken,
+        });
+      }
+    }
+  }
 
   const mount = async () => {
     // Event Bus Logger
