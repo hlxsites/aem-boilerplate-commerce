ece79fe87b88b956b2a829ef1fde618a4180a9b5
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-09-20 00:06:45 +0300

    Fix Lint

--- blocks/commerce-search-order/commerce-search-order.js
+++ blocks/commerce-search-order/commerce-search-order.js
@@ -1,74 +1,44 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
-import { SignIn } from "@dropins/storefront-auth/containers/SignIn.js";
-import { OrderSearch } from "@dropins/storefront-order/containers/OrderSearch.js";
-import { render as authRenderer } from "@dropins/storefront-auth/render.js";
-import { render as orderRenderer } from "@dropins/storefront-order/render.js";
-import { getCookie } from "../../scripts/configs.js";
+import { SignIn } from '@dropins/storefront-auth/containers/SignIn.js';
+import { OrderSearch } from '@dropins/storefront-order/containers/OrderSearch.js';
+import { render as authRenderer } from '@dropins/storefront-auth/render.js';
+import { render as orderRenderer } from '@dropins/storefront-order/render.js';
+import { getCookie } from '../../scripts/configs.js';
 
-const renderSignIn = async (element, email, orderNumber) =>
-  authRenderer.render(SignIn, {
-    initialEmailValue: email,
-    renderSignUpLink: false,
-    labels: {
-      formTitleText: email
-        ? "Enter your password to view order details"
-        : "Sign in to view order details",
-      primaryButtonText: "View order",
-    },
-    routeForgotPassword: () => "reset-password.html",
-    routeRedirectOnSignIn: () =>
-      `/customer/order-details?orderRef=${orderNumber}`,
-  })(element);
-
-function disableCacheForPage() {
-  const metaCacheControl = document.createElement("meta");
-  metaCacheControl.httpEquiv = "Cache-Control";
-  metaCacheControl.content = "no-cache, no-store, must-revalidate";
-
-  const metaPragma = document.createElement("meta");
-  metaPragma.httpEquiv = "Pragma";
-  metaPragma.content = "no-cache";
-
-  const metaExpires = document.createElement("meta");
-  metaExpires.httpEquiv = "Expires";
-  metaExpires.content = "0";
-
-  document.head.appendChild(metaCacheControl);
-  document.head.appendChild(metaPragma);
-  document.head.appendChild(metaExpires);
-}
-
-disableCacheForPage();
-
-const handleBeforeUnload = () => {
-  window.location.reload();
-};
-
-window.addEventListener("beforeunload", handleBeforeUnload);
-
-window.removeEventListener("beforeunload", handleBeforeUnload);
+const renderSignIn = async (element, email, orderNumber) => authRenderer.render(SignIn, {
+  initialEmailValue: email,
+  renderSignUpLink: false,
+  labels: {
+    formTitleText: email
+      ? 'Enter your password to view order details'
+      : 'Sign in to view order details',
+    primaryButtonText: 'View order',
+  },
+  routeForgotPassword: () => 'reset-password.html',
+  routeRedirectOnSignIn: () => `/customer/order-details?orderRef=${orderNumber}`,
+})(element);
 
 export default async function decorate(block) {
-  block.innerHTML = "";
+  block.innerHTML = '';
 
-  const isAuthenticated = !!getCookie("auth_dropin_user_token") || false;
+  const isAuthenticated = !!getCookie('auth_dropin_user_token') || false;
 
   await orderRenderer.render(OrderSearch, {
     isAuth: isAuthenticated,
     renderSignIn: async ({ render, formValues }) => {
       if (render) {
-        renderSignIn(block, formValues?.email ?? "", formValues?.number ?? "");
+        renderSignIn(block, formValues?.email ?? '', formValues?.number ?? '');
 
         return false;
       }
 
       return true;
     },
-    routeCustomerOrder: () => "/customer/order-details",
-    routeGuestOrder: () => "/order-details",
+    routeCustomerOrder: () => '/customer/order-details',
+    routeGuestOrder: () => '/order-details',
     onError: async (errorInformation) => {
-      console.info("errorInformation", errorInformation);
+      console.info('errorInformation', errorInformation);
     },
   })(block);
 }
