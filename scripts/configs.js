6da87287eb0471aeaf1333c59387c001f50ab6fb
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-09-19 18:04:28 +0300

    remove old code

--- scripts/configs.js
+++ scripts/configs.js
@@ -65,11 +65,6 @@ export const getConfigValue = async (configParam, environment) => {
   const configJSON = await getConfigForEnvironment(env);
   const configElements = JSON.parse(configJSON).data;
 
-  // if (configParam === "commerce-core-endpoint") {
-  //   // return "https://mcprod.aemshop.net/graphql";
-  //   return "https://mcstaging.aemshop.net/graphql";
-  // }
-
   return configElements.find((c) => c.key === configParam)?.value;
 };
 
