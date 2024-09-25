3d8686e0d0591b1ec28b4b6f11539ee65b424f9b
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-09-25 16:10:39 +0300

    eds-259 updated dropin js

--- scripts/dropins.js
+++ scripts/dropins.js
@@ -48,12 +48,8 @@ const handleUserOrdersRedirects = () => {
     return;
   }
 
-  events.on('order/error', ({ error }) => {
-    const defaultErrorMessage = "We couldn't locate an order with the information provided.";
-
-    if (error.includes(defaultErrorMessage)) {
-      window.location.href = `${ORDER_STATUS_PATH}`;
-    } else if (isAuthenticated) {
+  events.on('order/error', () => {
+    if (isAuthenticated) {
       window.location.href = `${CUSTOMER_ORDERS_PATH}`;
     } else {
       window.location.href = `${ORDER_STATUS_PATH}`;
