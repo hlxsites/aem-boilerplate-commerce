207dd1a3e1f705a03b9afc15d6ca5f8d54a2fd96
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2024-10-10 12:54:54 +0300

    eds-416 fixed error date

--- scripts/__dropins__/storefront-auth/i18n/en_US.json.d.ts
+++ scripts/__dropins__/storefront-auth/i18n/en_US.json.d.ts
@@ -39,6 +39,8 @@ declare const _default: {
       "emailError": "Please enter a valid email address.",
       "dateError": "Please enter a valid date.",
       "dateLengthError": "Date must be between {min} and {max}.",
+      "dateMaxError": "Date must be less than or equal to {max}.",
+      "dateMinError": "Date must be greater than or equal to {min}.",
       "urlError": "Please enter a valid URL, e.g., https://www.website.com.",
       "lengthTextError": "Text length must be between {min} and {max} characters."
     },
