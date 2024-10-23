40b142b37a02e3262402846218539cb1025fd305
Daniel <daniel.cini@thepixel.com>
2024-10-23 17:06:53 +0200

    Update product-showcase.js

--- blocks/product-showcase/product-showcase.js
+++ blocks/product-showcase/product-showcase.js
@@ -130,7 +130,7 @@ function renderProduct(product, config, block) {
     </div></a>
     <div class="details">
      <h1><a href="/products/${urlKey}/${sku}" aria-label="${name} product details">${name}</a></h1>
-      <a href=<div class="product-path">Shop Now</div>
+      <a href="/products/${urlKey}/${sku}" aria-label="${name} product details">${name}div class="product-path">Shop Now</a></div>
       <div class="actions">
         ${
           config['details-button']
