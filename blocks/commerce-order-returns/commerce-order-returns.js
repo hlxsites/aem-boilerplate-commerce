584095d65c2b5d8f313f1e38aece5a943ee896d2
Abrasimov Yaroslav <y.abrasimov@atwix.com>
2024-11-05 13:54:01 +0100

    Update return details URL

--- blocks/commerce-order-returns/commerce-order-returns.js
+++ blocks/commerce-order-returns/commerce-order-returns.js
@@ -2,16 +2,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { render as orderRenderer } from '@dropins/storefront-order/render.js';
 import { OrderReturns } from '@dropins/storefront-order/containers/OrderReturns.js';
+import { checkIsAuthenticated } from '../../scripts/configs.js';
+import {
+  CUSTOMER_RETURN_DETAILS_PATH,
+  RETURN_DETAILS_PATH,
+} from '../../scripts/constants.js';
 
 // Initialize
 import '../../scripts/initializers/order.js';
-import {
-  CUSTOMER_RETURN_DETAILS_PATH,
-} from '../../scripts/constants.js';
 
 export default async function decorate(block) {
+  const returnDetailsPath = checkIsAuthenticated()
+    ? CUSTOMER_RETURN_DETAILS_PATH
+    : RETURN_DETAILS_PATH;
+
   await orderRenderer.render(OrderReturns, {
-    routeReturnDetails: ({ orderNumber, returnNumber }) => `${CUSTOMER_RETURN_DETAILS_PATH}?orderRef=${orderNumber}&returnRef=${returnNumber}`,
+    routeReturnDetails: ({ orderNumber, returnNumber }) => `${returnDetailsPath}?orderRef=${orderNumber}&returnRef=${returnNumber}`,
     routeProductDetails: (productData) => (productData ? `/products/${productData.product.urlKey}/${productData.product.sku}` : '#'),
   })(block);
 }
