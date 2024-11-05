131517a42025afc4ee1498da8ffecfa8100dcdcb
Abrasimov Yaroslav <y.abrasimov@atwix.com>
2024-11-05 12:09:14 +0100

    Fix eslint issues

--- blocks/commerce-shipping-status/commerce-shipping-status.js
+++ blocks/commerce-shipping-status/commerce-shipping-status.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
-import { render as orderRenderer } from "@dropins/storefront-order/render.js";
-import { ShippingStatus } from "@dropins/storefront-order/containers/ShippingStatus.js";
+import { render as orderRenderer } from '@dropins/storefront-order/render.js';
+import { ShippingStatus } from '@dropins/storefront-order/containers/ShippingStatus.js';
 
 // Initialize
-import "../../scripts/initializers/order.js";
+import '../../scripts/initializers/order.js';
 
 export default async function decorate(block) {
   await orderRenderer.render(ShippingStatus, {
@@ -15,7 +15,7 @@ export default async function decorate(block) {
       if (data?.product) {
         return `/products/${data?.product?.urlKey}/${data?.product?.sku}`;
       }
-      return "#";
+      return '#';
     },
   })(block);
 }
