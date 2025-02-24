97f45f761ace82124d137da15a193fcba44c738e
Sergio Vera <svera@adobe.com>
2025-02-24 13:24:15 +0100

    LYNX-722: Force click on hidden elements

--- cypress/src/tests/e2eTests/verifyGuestUserCheckout.spec.js
+++ cypress/src/tests/e2eTests/verifyGuestUserCheckout.spec.js
@@ -31,7 +31,7 @@ describe('Verify guest user can place order', () => {
     cy.visit('');
     cy.get('.nav-drop')
       .contains('Catalog')
-      .click();
+      .click({force: true});
     cy.wait(1000);
     cy.contains('Crown Summit Backpack').click();
     cy.get('.dropin-incrementer__increase-button').click();
