e9d5531a6b0ddcafcc83a77e9353320d0af792cf
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2025-02-26 14:00:05 +0200

    updated pdp added api

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -253,7 +253,7 @@ export default async function decorate(block) {
             events.on(
               // @ts-ignore
               "cart/data",
-              (payload: any) => {
+              (payload) => {
                 const { items } = payload;
                 const dropinCartData = items.find(
                   (el) => el.sku === values.sku
