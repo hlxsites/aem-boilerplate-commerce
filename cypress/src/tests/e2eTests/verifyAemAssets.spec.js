048cda2980984ff396f23b8216a5d4805d5f45af
martinpa <martinpa@adobe.com>
2025-04-14 15:29:16 +0200

    Use should in tests

--- cypress/src/tests/e2eTests/verifyAemAssets.spec.js
+++ cypress/src/tests/e2eTests/verifyAemAssets.spec.js
@@ -35,40 +35,57 @@ describe('AEM Assets enabled', () => {
 
   it('should show optimized images when isAemAssetsEnabled is true', () => {
     cy.visit('/search?q=jacket');
-    cy.wait(5000);
-    cy.get('img').each(($img) => {
-      const src = $img.attr('src');
-      const urnMatch = src.match(/urn:aaid:aem:[^/]+/);
-      const imageNameMatch = src.match(/\/as\/([^/]+)\.webp\?/);
-      if (urnMatch && imageNameMatch) {
-        const urn = urnMatch[0];
-        const imageName = imageNameMatch[1];
-        const aemAssetsEnvironment = Cypress.env('AEM_ASSETS_ENVIRONMENT');
-        const expectedSrc = `http:////delivery-${aemAssetsEnvironment}.adobeaemcloud.com/adobe/assets/${urn}/as/${imageName}.webp?quality=80&width=200`;
-        expect(src).to.equal(expectedSrc);
+    cy.get('img').should(($imgs) => {
+      $imgs.each((_, img) => {
+        expect(img.complete).to.be.true
+        expect(img.naturalWidth).to.be.greaterThan(0)
+      })
+    }).then(($imgs) => {
+      const srcList = Cypress._.map($imgs, (img) => img.getAttribute('src'))
+
+      for (const src of srcList) {
+        const urnMatch = src.match(/urn:aaid:aem:[^/]+/);
+        const imageNameMatch = src.match(/\/as\/([^/]+)\.webp\?/);
+
+        if (urnMatch && imageNameMatch) {
+          const urn = urnMatch[0];
+          const imageName = imageNameMatch[1];
+          const aemAssetsEnvironment = Cypress.env('AEM_ASSETS_ENVIRONMENT');
+          const expectedSrc = `http:////delivery-${aemAssetsEnvironment}.adobeaemcloud.com/adobe/assets/${urn}/as/${imageName}.webp?quality=80&width=200`;
+
+          expect(src).to.equal(expectedSrc);
+        }
+
+        expect(src).to.include('/as/');
+        expect(src).not.to.include('fit=');
       }
-      expect(src).to.include('/as/');
-      cy.log($img);
-      expect(src).not.to.include('fit=');
-    });
+    })
   });
 });
 
 describe('AEM Assets disabled', () => {
   it('should show optimized images when isAemAssetsEnabled is false', () => {
     cy.visit('/search?q=jacket');
-    cy.wait(5000);
-    cy.get('img').each(($img) => {
-      const src = $img.attr('src');
-      const imageNameMatch = src.match(/\/w\/j\/([^/]+)\.jpg/);
-      if (imageNameMatch) {
-        const imageName = imageNameMatch[1];
-        const expectedSrc = `http:////mcstaging.aemshop.net/media/catalog/product/w/j/${imageName}.jpg?fit=cover&dpi=1&width=200`;
-        expect(src).to.equal(expectedSrc);
+    cy.get('img').should(($imgs) => {
+      $imgs.each((_, img) => {
+        expect(img.complete).to.be.true
+        expect(img.naturalWidth).to.be.greaterThan(0)
+      })
+    }).then(($imgs) => {
+      const srcList = Cypress._.map($imgs, (img) => img.getAttribute('src'))
+
+      for ( const src of srcList) {
+        const imageNameMatch = src.match(/\/w\/j\/([^/]+)\.jpg/);
+        if (imageNameMatch) {
+          const imageName = imageNameMatch[1];
+          const expectedSrc = `http:////mcstaging.aemshop.net/media/catalog/product/w/j/${imageName}.jpg?fit=cover&dpi=1&width=200`;
+
+          expect(src).to.equal(expectedSrc);
+        }
+
+        expect(src).not.to.include('/as/');
+        expect(src).to.include('fit=');
       }
-      expect(src).not.to.include('/as/');
-      cy.log($img);
-      expect(src).to.include('fit=');
     });
   });
 });
