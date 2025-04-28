2013883ff27afa1fd73c2b55cf28d8f00fe6760e
Rafal Janicki <rjanicki@adobe.com>
2025-04-28 17:01:52 +0100

    LYNX-844: Remove personalisation from AEM Boilerplate

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -12,7 +12,6 @@ import { loadFragment } from '../fragment/fragment.js';
 import renderAuthCombine from './renderAuthCombine.js';
 import { renderAuthDropdown } from './renderAuthDropdown.js';
 import { rootLink } from '../../scripts/scripts.js';
-import applyHashTagsForDomElement from '../../scripts/api/hashtags/api.js';
 
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
@@ -368,15 +367,3 @@ export default async function decorate(block) {
   );
   renderAuthDropdown(navTools);
 }
-
-events.on('cart/initialized', () => {
-  applyHashTagsForDomElement('nav');
-}, { eager: true });
-
-events.on('cart/updated', () => {
-  applyHashTagsForDomElement('nav');
-}, { eager: true });
-
-events.on('cart/reset', () => {
-  applyHashTagsForDomElement('nav');
-}, { eager: true });
