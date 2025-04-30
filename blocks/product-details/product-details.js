4aa21abf6883e5d9786e873930891e70502b4f72
Ivan Porto <iporto@adobe.com>
2025-04-30 18:43:47 +0200

    refactor: true passthrough on try methods and more robust config check

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -484,11 +484,6 @@ function imageSlotConfig(ctx) {
   const { data, defaultImageProps } = ctx;
   return {
     alias: data.sku,
-    src: defaultImageProps.src,
     imageProps: defaultImageProps,
-    params: {
-      width: defaultImageProps.width,
-      height: defaultImageProps.height,
-    },
   };
 }
