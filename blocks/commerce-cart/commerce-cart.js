d46957b9a9b6415a031dcdb0a53f6deace198418
Ivan Porto <iporto@adobe.com>
2025-05-05 09:55:40 +0200

    refactor: consistent image slots

--- blocks/commerce-cart/commerce-cart.js
+++ blocks/commerce-cart/commerce-cart.js
@@ -104,13 +104,7 @@ export default async function decorate(block) {
 
           tryRenderAemAssetsImage(ctx, {
             alias: item.sku,
-            src: defaultImageProps.src,
             imageProps: defaultImageProps,
-            params: {
-              width: defaultImageProps.width,
-              height: defaultImageProps.height,
-            },
-
             wrapper: anchorWrapper,
           });
         },
@@ -163,13 +157,7 @@ export default async function decorate(block) {
                 const { defaultImageProps, imageSwatchContext } = swatchCtx;
                 tryRenderAemAssetsImage(swatchCtx, {
                   alias: imageSwatchContext.label,
-                  src: defaultImageProps.src,
                   imageProps: defaultImageProps,
-                  params: {
-                    width: defaultImageProps.width,
-                    height: defaultImageProps.height,
-                  },
-
                   wrapper: document.createElement('span'),
                 });
               },
@@ -221,9 +209,9 @@ export default async function decorate(block) {
 
       slots: {
         SwatchImage: (ctx) => {
-          const { defaultImageProps } = ctx;
+          const { imageSwatchContext, defaultImageProps } = ctx;
           tryRenderAemAssetsImage(ctx, {
-            alias: 'gift-wrapping',
+            alias: imageSwatchContext.label,
             imageProps: defaultImageProps,
             wrapper: document.createElement('span'),
           });
