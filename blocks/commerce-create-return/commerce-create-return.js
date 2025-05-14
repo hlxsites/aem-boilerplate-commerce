e2a8ad46d2b6d2193c2e7a72d8e347d02df195c8
akhyatelachl <akhyatelachl@adobe.com>
2025-05-14 11:24:15 +0200

    add asset image rendering to order returns

--- blocks/commerce-create-return/commerce-create-return.js
+++ blocks/commerce-create-return/commerce-create-return.js
@@ -5,12 +5,29 @@ import { CreateReturn } from '@dropins/storefront-order/containers/CreateReturn.
 import { checkIsAuthenticated } from '../../scripts/configs.js';
 import { ORDER_DETAILS_PATH, CUSTOMER_ORDER_DETAILS_PATH } from '../../scripts/constants.js';
 import { rootLink } from '../../scripts/scripts.js';
+import { tryRenderAemAssetsImage } from '../../scripts/assets.js';
 
 // Initialize
 import '../../scripts/initializers/order.js';
 
 export default async function decorate(block) {
   await orderRenderer.render(CreateReturn, {
+    slots: {
+      ReturnReasonFormImage: (ctx) => {
+        const { data, defaultImageProps } = ctx;
+        tryRenderAemAssetsImage(ctx, {
+          alias: data.product.sku,
+          imageProps: defaultImageProps,
+        });
+      },
+      CartSummaryItemImage: (ctx) => {
+        const { data, defaultImageProps } = ctx;
+        tryRenderAemAssetsImage(ctx, {
+          alias: data.product.sku,
+          imageProps: defaultImageProps,
+        });
+      },
+    },
     routeReturnSuccess: (orderData) => {
       const orderRef = checkIsAuthenticated() ? orderData.number : orderData.token;
       const encodedOrderRef = encodeURIComponent(orderRef);
