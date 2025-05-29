36642027d07914d40ac0ce52412161cf718d96d3
Ivan Porto Wigner <dev.ivanporto@gmail.com>
2025-05-29 09:52:56 +0200

    ACAP-1004: Enable AEM Assets in Cart (#428)
    * ACAP-829: Enable AEM Assets in PDP (#420)
    
    * chore: update pdp to 1.2.0-beta1
    
    * chore: bump storefront sdk to v1.2.0
    
    * feat: add support for AEM Assets images in PDP
    
    * chore: bump pdp version to 1.2.0-beta2
    
    * chore: pin sdk to 1.2.0
    
    * test: add pdp aem assets tests
    
    * refactor: try generate assets url passthrough for non aem assets + swatch image slot
    
    * refactor: complete jsdoc
    
    * refactor: streamline image slot config
    
    * refactor: remove wrapper from slot config maker
    
    * feat: add cart thumbnail slot
    
    * feat: add mini cart thumbnail slot
    
    * feat: add gift options swatch image slot
    
    * test: add cart aem assets tests
    
    * test: skip aem assets tests
    
    * refactor: fix linting issues
    
    * refactor: use destructured item instead of ctx
    
    * chore: sync with latest ACAP-1017
    
    * fix: swatch image slots
    
    * fix: sync assets script and lint issues
    
    * fix: newline end assets scripts
    
    * refactor: consistent image slots
    
    * refactor: extract swatch image slot to reusable function

--- blocks/commerce-cart/commerce-cart.js
+++ blocks/commerce-cart/commerce-cart.js
@@ -21,6 +21,7 @@ import '../../scripts/initializers/cart.js';
 import { readBlockConfig } from '../../scripts/aem.js';
 import { rootLink } from '../../scripts/scripts.js';
 import { fetchPlaceholders } from '../../scripts/commerce.js';
+import { tryRenderAemAssetsImage } from '../../scripts/assets.js';
 
 export default async function decorate(block) {
   // Configuration
@@ -82,11 +83,12 @@ export default async function decorate(block) {
   toggleEmptyCart(isEmptyCart);
 
   // Render Containers
+  const productLink = (product) => rootLink(`/products/${product.url.urlKey}/${product.topLevelSku}`);
   await Promise.all([
     // Cart List
     provider.render(CartSummaryList, {
       hideHeading: hideHeading === 'true',
-      routeProduct: (product) => rootLink(`/products/${product.url.urlKey}/${product.topLevelSku}`),
+      routeProduct: productLink,
       routeEmptyCartCTA: startShoppingURL ? () => rootLink(startShoppingURL) : undefined,
       maxItems: parseInt(maxItems, 10) || undefined,
       attributesToHide: hideAttributes
@@ -95,6 +97,18 @@ export default async function decorate(block) {
       enableUpdateItemQuantity: enableUpdateItemQuantity === 'true',
       enableRemoveItem: enableRemoveItem === 'true',
       slots: {
+        Thumbnail: (ctx) => {
+          const { item, defaultImageProps } = ctx;
+          const anchorWrapper = document.createElement('a');
+          anchorWrapper.href = productLink(item);
+
+          tryRenderAemAssetsImage(ctx, {
+            alias: item.sku,
+            imageProps: defaultImageProps,
+            wrapper: anchorWrapper,
+          });
+        },
+
         Footer: (ctx) => {
           const giftOptions = document.createElement('div');
 
@@ -138,6 +152,9 @@ export default async function decorate(block) {
             handleItemsLoading: ctx.handleItemsLoading,
             handleItemsError: ctx.handleItemsError,
             onItemUpdate: ctx.onItemUpdate,
+            slots: {
+              SwatchImage: swatchImageSlot,
+            },
           })(giftOptions);
 
           ctx.appendChild(giftOptions);
@@ -147,7 +164,7 @@ export default async function decorate(block) {
 
     // Order Summary
     provider.render(OrderSummary, {
-      routeProduct: (product) => rootLink(`/products/${product.url.urlKey}/${product.topLevelSku}`),
+      routeProduct: productLink,
       routeCheckout: checkoutURL ? () => rootLink(checkoutURL) : undefined,
       slots: {
         EstimateShipping: async (ctx) => {
@@ -182,6 +199,10 @@ export default async function decorate(block) {
     provider.render(GiftOptions, {
       view: 'order',
       dataSource: 'cart',
+
+      slots: {
+        SwatchImage: swatchImageSlot,
+      },
     })($giftOptions),
   ]);
 
@@ -235,4 +256,13 @@ export default async function decorate(block) {
 
 function isCartEmpty(cart) {
   return cart ? cart.totalQuantity < 1 : true;
+}
+
+function swatchImageSlot(ctx) {
+  const { imageSwatchContext, defaultImageProps } = ctx;
+  tryRenderAemAssetsImage(ctx, {
+    alias: imageSwatchContext.label,
+    imageProps: defaultImageProps,
+    wrapper: document.createElement('span'),
+  });
 }
