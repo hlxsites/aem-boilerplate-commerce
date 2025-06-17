f96f877c0d5a9b5069410068ac2c721e98b36b2f
Sergio Vera <svera@adobe.com>
2025-06-17 15:38:12 +0200

    LYNX-899: Pass optionUIDs and remove condition tt render toggle only with simple products

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -333,7 +333,7 @@ export default async function decorate(block) {
   }
 
   events.on('cart/updated', async (data) => {
-    const item = getWishlistItemFromStorage(product.sku);
+    const item = getWishlistItemFromStorage(product.sku, product.optionUIDs);
     if (!item) {
       return;
     }
