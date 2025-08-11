5aba67d73d94ad008e74fb4528679e10aa9a70fa
Carlos A. Cabrera <cacabrer@adobe.com>
2025-08-11 09:15:27 -0500

    Remove global exposure of events in header JavaScript to improve encapsulation and maintainability.

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -17,7 +17,6 @@ const labels = await fetchPlaceholders();
 const overlay = document.createElement('div');
 overlay.classList.add('overlay');
 document.querySelector('header').insertAdjacentElement('afterbegin', overlay);
-window.events = events;
 
 function closeOnEscape(e) {
   if (e.code === 'Escape') {
