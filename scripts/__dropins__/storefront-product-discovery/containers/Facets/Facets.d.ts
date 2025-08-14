281cb9b092d2dd3d6538ef388884d07dac0177f9
Carlos A. Cabrera <cacabrer@adobe.com>
2025-08-13 19:07:30 -0500

    Update storefront-product-discovery to version 2.0.0-alpha11 and remove unused rootCategory prop from Facets component. Simplified Facets rendering by eliminating the rootCategory parameter.

--- scripts/__dropins__/storefront-product-discovery/containers/Facets/Facets.d.ts
+++ scripts/__dropins__/storefront-product-discovery/containers/Facets/Facets.d.ts
@@ -3,7 +3,6 @@ import { Container, SlotProps } from '@dropins/tools/types/elsie/src/lib';
 import { SearchFacet, FacetBucket, Scope } from '../../data/models/api';
 
 export interface FacetsProps extends HTMLAttributes<HTMLDivElement> {
-    rootCategory?: string;
     scope?: Scope;
     slots?: {
         Facet?: SlotProps<{
