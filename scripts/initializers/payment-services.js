a1eb232e2cc4e4005f577d2a27c9f6fe14612ba6
Jordi Ortolá Ankum <jortolaankum@adobe.com>
2025-08-26 17:53:29 +0200

    INTERNAL: Move placeholders from drafts to root

--- scripts/initializers/payment-services.js
+++ scripts/initializers/payment-services.js
@@ -4,9 +4,7 @@ import { initializeDropin } from './index.js';
 import { fetchPlaceholders } from '../commerce.js';
 
 await initializeDropin(async () => {
-  // TODO(jortola): Once approved, copy payment-services.json file in Document Authoring to
-  //            ... /placeholders/payment-services.json update path below accordingly.
-  const labels = await fetchPlaceholders('drafts/jortola/placeholders/payment-services.json');
+  const labels = await fetchPlaceholders('placeholders/payment-services.json');
   const langDefinitions = {
     default: {
       ...labels,
