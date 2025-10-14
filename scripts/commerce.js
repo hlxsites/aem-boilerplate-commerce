70aab496003e73a030f50e72d65b47c9fe95b156
Paras Sood <paras89@live.com>
2025-10-14 12:14:31 +0100

    USF-2668 Company Registration: Fix lint error

--- scripts/commerce.js
+++ scripts/commerce.js
@@ -580,10 +580,6 @@ function getSkuFromUrl() {
   return result?.[1];
 }
 
-export function getProductSku() {
-  return getMetadata('sku') || getSkuFromUrl();
-}
-
 export function getProductLink(urlKey, sku) {
   return rootLink(`/products/${urlKey}/${sku}`.toLowerCase());
 }
