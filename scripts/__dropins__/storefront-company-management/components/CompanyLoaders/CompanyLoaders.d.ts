6ff7cdf8a078719acf9e1eb34d3e7c848c5c172a
Kevin Kozan <kkozan@magento.com>
2025-10-20 14:44:59 -0500

    USF-2674: Company User can see Company Credit Details
    - Dropin code added.

---
+++ scripts/__dropins__/storefront-company-management/components/CompanyLoaders/CompanyLoaders.d.ts
@@ -0,0 +1,16 @@
+
+export declare const CompanyCardLoader: ({ testId, withCard, }: {
+    testId?: string | undefined;
+    withCard?: boolean | undefined;
+}) => import("preact").JSX.Element;
+export declare const CompanyUsersTableLoader: ({ testId }: {
+    testId?: string | undefined;
+}) => import("preact").JSX.Element;
+export declare const CompanyCreditLoader: ({ testId }: {
+    testId?: string | undefined;
+}) => import("preact").JSX.Element;
+export declare const CompanyCreditHistoryLoader: ({ testId }: {
+    testId?: string | undefined;
+}) => import("preact").JSX.Element;
+export declare const CompanyUserFormLoader: () => import("preact").JSX.Element;
+//# sourceMappingURL=CompanyLoaders.d.ts.map
