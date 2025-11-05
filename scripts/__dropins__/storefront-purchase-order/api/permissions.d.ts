abac47b65d9303f6e991c33c07987e2d6d399268
Konstantin Fandelyuk <k.fandeliuk@atwix.com>
2025-11-05 13:09:52 +0200

    feat: add permissions module and update API exports

---
+++ scripts/__dropins__/storefront-purchase-order/api/permissions.d.ts
@@ -0,0 +1,11 @@
+export declare const PO_PERMISSIONS: {
+    PO_ALL: string;
+    VIEW_CUSTOMER: string;
+    VIEW_SUBORDINATES: string;
+    VIEW_COMPANY: string;
+    AUTO_APPROVE: string;
+    SUPER_APPROVE: string;
+    VIEW_RULES: string;
+    MANAGE_RULES: string;
+};
+//# sourceMappingURL=permissions.d.ts.map
