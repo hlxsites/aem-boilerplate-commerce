23e05537e8dcdffcc0e93906c72010d8f51efdc9
merino <merino@adobe.com>
2025-11-14 16:33:15 +0100

    Revert payment-services changes in block

--- blocks/commerce-checkout/commerce-checkout.js
+++ blocks/commerce-checkout/commerce-checkout.js
@@ -75,6 +75,7 @@ import { rootLink } from '../../scripts/commerce.js';
 import '../../scripts/initializers/account.js';
 import '../../scripts/initializers/checkout.js';
 import '../../scripts/initializers/order.js';
+import '../../scripts/initializers/payment-services.js';
 
 // Checkout success block import and CSS preload
 import { renderCheckoutSuccess, preloadCheckoutSuccess } from '../commerce-checkout-success/commerce-checkout-success.js';
