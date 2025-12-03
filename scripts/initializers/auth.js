5e2a73585fabb754caf767b448ce852942710768
Carlos A. Cabrera <cacabrer@adobe.com>
2025-12-03 12:15:00 -0600

    Refactor authentication initializer to dynamically set adobeCommerceOptimizer based on configuration

--- scripts/initializers/auth.js
+++ scripts/initializers/auth.js
@@ -1,9 +1,12 @@
 import { initializers } from '@dropins/tools/initializer.js';
+import { getConfigValue } from '@dropins/tools/lib/aem/configs.js';
 import { initialize, setEndpoint } from '@dropins/storefront-auth/api.js';
 import { initializeDropin } from './index.js';
 import { CORE_FETCH_GRAPHQL, fetchPlaceholders } from '../commerce.js';
 
 await initializeDropin(async () => {
+  const adobeCommerceOptimizer = getConfigValue('adobe-commerce-optimizer') || false;
+
   // Set Fetch GraphQL (Core)
   setEndpoint(CORE_FETCH_GRAPHQL);
 
@@ -16,8 +19,5 @@ await initializeDropin(async () => {
   };
 
   // Initialize auth
-  return initializers.mountImmediately(initialize, {
-    langDefinitions,
-    adobeCommerceOptimizer: true,
-  });
+  return initializers.mountImmediately(initialize, { langDefinitions, adobeCommerceOptimizer });
 })();
