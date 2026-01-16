dfa7ce617dbd574c4cc9fe52e40d80594efc17a2
Dev Patil <depatil@adobe.com>
2026-01-16 13:45:46 -0600

    fix merge conflicts

--- blocks/commerce-checkout/commerce-checkout.js
+++ blocks/commerce-checkout/commerce-checkout.js
@@ -21,8 +21,7 @@ import { PaymentMethodCode } from '@dropins/storefront-payment-services/api.js';
 
 // Block Utilities
 import { getConfigValue } from '@dropins/tools/lib/aem/configs.js';
-import { getUserTokenCookie } from '../../scripts/initializers/index.js';
-import { displayOverlaySpinner, removeModal, removeOverlaySpinner } from './utils.js';
+import { buildOrderDetailsUrl, displayOverlaySpinner, removeOverlaySpinner } from './utils.js';
 
 // Fragment functions
 import { createCheckoutFragment, selectors } from './fragments.js';
@@ -192,7 +191,7 @@ export default async function decorate(block) {
   };
 
   // First, render the place order component
-  await renderPlaceOrder($placeOrder, { handleValidation, handlePlaceOrder,  b2bIsPoEnabled });
+  await renderPlaceOrder($placeOrder, { handleValidation, handlePlaceOrder, b2bIsPoEnabled });
 
   // Render the remaining containers
   const [
