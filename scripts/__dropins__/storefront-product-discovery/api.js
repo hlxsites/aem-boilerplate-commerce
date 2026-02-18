7fbf19a26a9c7a3833beb3f641695050bf5fc00c
Dev Patil <depatil@adobe.com>
2026-02-18 10:47:49 -0600

    update search dropin

--- scripts/__dropins__/storefront-product-discovery/api.js
+++ scripts/__dropins__/storefront-product-discovery/api.js
@@ -1,4 +1,4 @@
 /*! Copyright 2026 Adobe
 All Rights Reserved. */
-import{f as p,c as h,g as c,r as m,d,s as f,a as l,b as g}from"./chunks/search.js";import{c as G,i as Q}from"./chunks/initialize.js";import"@dropins/tools/lib.js";import"@dropins/tools/event-bus.js";import"./fragments.js";import"./chunks/acdlEvents.js";import"@dropins/tools/fetch-graphql.js";export{G as config,p as fetchGraphQl,h as getConfig,c as getFetchGraphQlHeader,Q as initialize,m as removeFetchGraphQlHeader,d as search,f as setEndpoint,l as setFetchGraphQlHeader,g as setFetchGraphQlHeaders};
+import{f as p,d as h,c,g as m,r as g,e as d,s as f,a as l,b as n}from"./chunks/search.js";import{c as G,i as Q}from"./chunks/initialize.js";import"@dropins/tools/lib.js";import"@dropins/tools/event-bus.js";import"./fragments.js";import"./chunks/acdlEvents.js";import"@dropins/tools/fetch-graphql.js";export{G as config,p as fetchGraphQl,h as getCategoryFromPath,c as getConfig,m as getFetchGraphQlHeader,Q as initialize,g as removeFetchGraphQlHeader,d as search,f as setEndpoint,l as setFetchGraphQlHeader,n as setFetchGraphQlHeaders};
 //# sourceMappingURL=api.js.map
--- scripts/__dropins__/storefront-product-discovery/api/search/search.d.ts
+++ scripts/__dropins__/storefront-product-discovery/api/search/search.d.ts
@@ -4,6 +4,7 @@ type SearchOptions = {
     scope?: Scope;
     searchPathname?: string;
 };
+export declare const getCategoryFromPath: (scope: string | undefined, searchPath: string) => string | null | undefined;
 export declare const search: (request: SearchVariables | null, options?: SearchOptions) => Promise<ProductSearchResult>;
 export {};
 //# sourceMappingURL=search.d.ts.map
