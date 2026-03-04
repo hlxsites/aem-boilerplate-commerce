06b2b2311d7bd195109d5b8f8f648a4c2aa8339c
chutingy <chutingy@adobe.com>
2026-03-04 15:53:27 -0600

    use prop in mini cart

--- blocks/commerce-mini-cart/commerce-mini-cart.js
+++ blocks/commerce-mini-cart/commerce-mini-cart.js
@@ -26,6 +26,7 @@ export default async function decorate(block) {
     'checkout-url': checkoutURL = '',
     'enable-updating-product': enableUpdatingProduct = 'false',
     'undo-remove-item': undo = 'false',
+    'show-inventory-issue-items-in-main-list': showInventoryIssueItemsInMainList = 'false',
   } = readBlockConfig(block);
 
   // Get translations for custom messages
@@ -167,6 +168,7 @@ export default async function decorate(block) {
     routeCheckout: checkoutURL ? () => rootLink(checkoutURL) : undefined,
     routeProduct: createProductLink,
     undo: undo === 'true',
+    showInventoryIssueItemsInMainList: showInventoryIssueItemsInMainList === 'true',
 
     slots: {
       Thumbnail: (ctx) => {
