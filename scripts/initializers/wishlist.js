e6b255e6adbf2a309567beef5064428a937ebe27
merino <merino@adobe.com>
2026-03-05 17:23:01 +0100

    Use getHeaders to retrieve store code

--- scripts/initializers/wishlist.js
+++ scripts/initializers/wishlist.js
@@ -1,6 +1,6 @@
 import { initializers } from '@dropins/tools/initializer.js';
 import { initialize, setEndpoint } from '@dropins/storefront-wishlist/api.js';
-import { getConfigValue, getHeaders } from '@dropins/tools/lib/aem/configs.js';
+import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import { initializeDropin } from './index.js';
 import { CORE_FETCH_GRAPHQL, fetchPlaceholders } from '../commerce.js';
 
