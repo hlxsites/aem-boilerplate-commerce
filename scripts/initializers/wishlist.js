3ed87d6d4d3a729777c1bb10be4c1ce5aaa151d1
merino <merino@adobe.com>
2026-03-05 17:21:31 +0100

    Use getHeaders to retrieve store code

--- scripts/initializers/wishlist.js
+++ scripts/initializers/wishlist.js
@@ -1,6 +1,6 @@
 import { initializers } from '@dropins/tools/initializer.js';
 import { initialize, setEndpoint } from '@dropins/storefront-wishlist/api.js';
-import { getConfigValue } from '@dropins/tools/lib/aem/configs.js';
+import { getConfigValue, getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import { initializeDropin } from './index.js';
 import { CORE_FETCH_GRAPHQL, fetchPlaceholders } from '../commerce.js';
 
@@ -8,6 +8,7 @@ await initializeDropin(async () => {
   // Set Fetch GraphQL (Catalog Service)
   setEndpoint(CORE_FETCH_GRAPHQL);
 
+  const headers = getHeaders('wishlist');
   // Fetch placeholders
   const labels = await fetchPlaceholders('placeholders/wishlist.json');
   const langDefinitions = {
@@ -16,12 +17,10 @@ await initializeDropin(async () => {
     },
   };
 
-  const storeCode = getConfigValue('headers.cs.Magento-Store-View-Code');
-
   // Initialize wishlist
   return initializers.mountImmediately(initialize, {
     langDefinitions,
     isGuestWishlistEnabled: true,
-    storeCode,
+    storeCode: headers.Store,
   });
 })();
