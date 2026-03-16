099bc73fcd35a905a7a897cd7c0c5b6e850db42d
Eric Oeur <oeur@mac.lan>
2026-03-16 16:57:19 -0500

    updated alpha package for storefront-rec

--- scripts/__dropins__/storefront-requisition-list/chunks/getRequisitionLists.js
+++ scripts/__dropins__/storefront-requisition-list/chunks/getRequisitionLists.js
@@ -1,6 +1,6 @@
 /*! Copyright 2026 Adobe
 All Rights Reserved. */
-import{R as S,a as T,f as c,h as E,t as g}from"./updateRequisitionList.js";import{events as m}from"@dropins/tools/event-bus.js";const p=`
+import{R as S,a as T,t as c}from"./updateRequisitionList.js";import{f as m,h as E}from"./fetch-graphql.js";import{events as g}from"@dropins/tools/event-bus.js";const p=`
   query GET_REQUISITION_LISTS_QUERY(
     $currentPage: Int = 1
     $pageSize: Int = 10,
@@ -24,5 +24,5 @@ import{R as S,a as T,f as c,h as E,t as g}from"./updateRequisitionList.js";impor
   }
 ${S}
 ${T}
-`,f=async(I,u)=>c(p,{variables:{currentPage:I,pageSize:u}}).then(({errors:t,data:e})=>{var s,r,n,o,_;if(t)return E(t);if(!((s=e==null?void 0:e.customer)!=null&&s.requisition_lists))return null;const i=e.customer.requisition_lists.items.map(a=>g(a));return m.emit("requisitionLists/data",i),{items:i,page_info:(n=(r=e.customer)==null?void 0:r.requisition_lists)==null?void 0:n.page_info,total_count:(_=(o=e.customer)==null?void 0:o.requisition_lists)==null?void 0:_.total_count}});export{f as g};
+`,q=async(I,u)=>m(p,{variables:{currentPage:I,pageSize:u}}).then(({errors:e,data:t})=>{var s,r,n,o,_;if(e)return E(e);if(!((s=t==null?void 0:t.customer)!=null&&s.requisition_lists))return null;const i=t.customer.requisition_lists.items.map(a=>c(a));return g.emit("requisitionLists/data",i),{items:i,page_info:(n=(r=t.customer)==null?void 0:r.requisition_lists)==null?void 0:n.page_info,total_count:(_=(o=t.customer)==null?void 0:o.requisition_lists)==null?void 0:_.total_count}});export{q as g};
 //# sourceMappingURL=getRequisitionLists.js.map
