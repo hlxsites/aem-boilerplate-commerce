b670c2b320e9eff05949605b11caf2f597e41d4c
Abrasimov Yaroslav <y.abrasimov@atwix.com>
2026-03-17 13:57:33 +0100

    Install alpha versions of auth and account

--- blocks/commerce-seller-assisted-buying/commerce-seller-assisted-buying.js
+++ blocks/commerce-seller-assisted-buying/commerce-seller-assisted-buying.js
@@ -1,6 +1,4 @@
 import { render as accountRenderer } from '@dropins/storefront-account/render.js';
-// TODO: Remove comment when version will be published
-// eslint-disable-next-line
 import { SellerAssistedPurchasing } from '@dropins/storefront-account/containers/SellerAssistedPurchasing.js';
 
 // Initialize
