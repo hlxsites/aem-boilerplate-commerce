8533296b8ac8b782646fe5a3a2a31574d19c3793
Dev Patil <depatil@adobe.com>
2026-03-25 18:10:38 -0500

    Update ACCS search filters

--- scripts/__dropins__/storefront-product-discovery/containers/Facets/utils/facetUtils.d.ts
+++ scripts/__dropins__/storefront-product-discovery/containers/Facets/utils/facetUtils.d.ts
@@ -1,4 +1,4 @@
-import { SearchFacet } from '../../../data/models/api';
+import { SearchFacet, SearchFilter } from '../../../data/models/api';
 import { Product } from '../../../data/models/product';
 
 declare const getFilterSelection: (facet: SearchFacet) => {
@@ -11,7 +11,12 @@ declare const getFilterSelection: (facet: SearchFacet) => {
     attribute: string;
     in: string[];
 } | null | undefined;
+/**
+ * Returns whether a facet bucket is selected according to the given filter.
+ * Handles range buckets, scalar/category in-list selection, and category parent selection when a child is selected.
+ */
+declare const isBucketSelected: (bucket: any, facet: SearchFacet, selectedFilter: SearchFilter | undefined) => boolean;
 declare const getCurrencyType: (items: Product[]) => string;
 declare const getBucketLabel: (bucket: any, facet: SearchFacet, currency: string | null) => any;
-export { getBucketLabel, getCurrencyType, getFilterSelection };
+export { getBucketLabel, getCurrencyType, getFilterSelection, isBucketSelected };
 //# sourceMappingURL=facetUtils.d.ts.map
