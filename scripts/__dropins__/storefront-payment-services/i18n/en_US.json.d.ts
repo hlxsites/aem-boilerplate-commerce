4b2f750221867f2ffe510d035973cf08b5cd6140
Anthoula Wojczak <awojczak@adobe.com>
2026-03-26 08:29:52 -0500

    add save card field during checkout

--- scripts/__dropins__/storefront-payment-services/i18n/en_US.json.d.ts
+++ scripts/__dropins__/storefront-payment-services/i18n/en_US.json.d.ts
@@ -15,6 +15,9 @@ declare const _default: {
           "message": "An unexpected error occurred. Please try again or contact support."
         }
       },
+      "saveCard": {
+        "label": "Save this card for future purchases"
+      },
       "formFields": {
         "cvv": {
           "invalidError": "Enter valid cvv.",
