1f38f205b00b0516e93a3848053b8d0b8ae43ecd
Elisea Cornejo <ecornejo@adobe.com>
2026-04-02 15:14:34 +0200

    fix flaky tests

--- cypress/src/support/b2bLoginHelpers.js
+++ cypress/src/support/b2bLoginHelpers.js
@@ -37,9 +37,9 @@ Cypress.Commands.add('loginAsCompanyAdmin', () => {
     cy.wait(1000); // Ensure page is ready
 
     cy.get('main .auth-sign-in-form', { timeout: 10000 }).within(() => {
-      cy.get('input[name="email"]').type(testAdmin.email);
+      cy.get('input[name="email"]').should('be.visible').and('not.be.disabled').type(testAdmin.email);
       cy.wait(1500);
-      cy.get('input[name="password"]').type(testAdmin.password);
+      cy.get('input[name="password"]').should('be.visible').and('not.be.disabled').type(testAdmin.password);
       cy.wait(1500);
       cy.get('button[type="submit"]').click();
     });
@@ -69,9 +69,9 @@ Cypress.Commands.add('loginAsRegularUser', () => {
     cy.wait(1000); // Ensure page is ready
 
     cy.get('main .auth-sign-in-form', { timeout: 10000 }).within(() => {
-      cy.get('input[name="email"]').type(testUsers.regular.email);
+      cy.get('input[name="email"]').should('be.visible').and('not.be.disabled').type(testUsers.regular.email);
       cy.wait(1500);
-      cy.get('input[name="password"]').type(testUsers.regular.password);
+      cy.get('input[name="password"]').should('be.visible').and('not.be.disabled').type(testUsers.regular.password);
       cy.wait(1500);
       cy.get('button[type="submit"]').click();
     });
@@ -118,9 +118,9 @@ Cypress.Commands.add('loginAsRestrictedUser', () => {
     cy.wait(1000); // Ensure page is ready
     
     cy.get('main .auth-sign-in-form', { timeout: 10000 }).within(() => {
-      cy.get('input[name="email"]').type(testUsers.restricted.email);
+      cy.get('input[name="email"]').should('be.visible').and('not.be.disabled').type(testUsers.restricted.email);
       cy.wait(1500);
-      cy.get('input[name="password"]').type(testUsers.restricted.password);
+      cy.get('input[name="password"]').should('be.visible').and('not.be.disabled').type(testUsers.restricted.password);
       cy.wait(1500);
       cy.get('button[type="submit"]').click();
     });
