79ef9d325d37750ef9245d8692376566e117a3e6
Jaume Capdevila <capdevil@adobe.com>
2026-04-21 17:19:55 +0200

    fix(delayedType): update default delay to 150ms for typing

--- cypress/src/support/delayedType.js
+++ cypress/src/support/delayedType.js
@@ -3,13 +3,14 @@
  *
  * @param {string} selector - The CSS selector for the input element
  * @param {string} value - The text to type
- * @param {number} [delay=50] - Delay between keystrokes in ms
+ * @param {number} [delay=150] - Delay between keystrokes in ms
  *
  * @example
  * cy.delayedType('input[name="email"]', 'user@example.com');
  * cy.delayedType('input[name="email"]', 'user@example.com', 100);
  */
-Cypress.Commands.add('delayedType', (selector, value, delay = 100) => {
+Cypress.Commands.add('delayedType', (selector, value, delay = 150) => {
   cy.get(selector).clear();
   cy.get(selector).type(value, { delay });
+  cy.get(selector).should('have.value', value);
 });
