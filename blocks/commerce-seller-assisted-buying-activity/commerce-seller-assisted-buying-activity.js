b412faa2d6753bd01e81f8ce4736b43f7723b695
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2026-05-05 15:44:48 +0200

    Revert blocks (miss-use)

--- blocks/commerce-seller-assisted-buying-activity/commerce-seller-assisted-buying-activity.js
+++ blocks/commerce-seller-assisted-buying-activity/commerce-seller-assisted-buying-activity.js
@@ -1,5 +1,5 @@
 import { render as accountRenderer } from '@dropins/storefront-account/render.js';
-import { SellerAssistedBuyingSettings } from '@dropins/storefront-account/containers/SellerAssistedBuyingSettings.js';
+import { SellerAssistedBuyingActivity } from '@dropins/storefront-account/containers/SellerAssistedBuyingActivity.js';
 import { CUSTOMER_LOGIN_PATH, checkIsAuthenticated, rootLink } from '../../scripts/commerce.js';
 
 // Initialize
@@ -10,7 +10,7 @@ export default async function decorate(block) {
     window.location.href = rootLink(CUSTOMER_LOGIN_PATH);
   } else {
     const container = document.createElement('div');
-    await accountRenderer.render(SellerAssistedBuyingSettings, {})(container);
+    await accountRenderer.render(SellerAssistedBuyingActivity, {})(container);
     block.appendChild(container);
   }
 }
