0fd8e2ad12d03caee338a10a4272560dbc2f0a85
dherrero2 <dherrero@adobe.com>
2026-07-23 18:26:41 +0200

    PAY-6081: Include scripts of new drop-in version (#1356)
    * Update scripts folder with npm install
    
    * Update scripts folder with npm install

--- scripts/__dropins__/storefront-payment-services/containers/PayPalButtons/index.d.ts
+++ scripts/__dropins__/storefront-payment-services/containers/PayPalButtons/index.d.ts
@@ -2,7 +2,7 @@
  * ADOBE CONFIDENTIAL
  * __________________
  *
- *  Copyright 2024 Adobe
+ *  Copyright 2026 Adobe
  *  All Rights Reserved.
  *
  * NOTICE:  All information contained herein is, and remains
@@ -14,8 +14,6 @@
  * is strictly forbidden unless prior written permission is obtained
  * from Adobe.
  *******************************************************************/
-export * from './CreditCard/CreditCard';
-export * from './ApplePay/ApplePay';
-export * from './GooglePay/GooglePay';
-export * from './PayPalButtons/PayPalButtons';
+export * from './PayPalButtons';
+export { PayPalButtons as default } from './PayPalButtons';
 //# sourceMappingURL=index.d.ts.map
