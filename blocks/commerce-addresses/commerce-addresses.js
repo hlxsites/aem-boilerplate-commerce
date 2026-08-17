fd4a506972bbc3a39bd3072dc6743907bc11024b
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2026-08-17 16:32:27 +0200

    Draft conditional Addresses title

--- blocks/commerce-addresses/commerce-addresses.js
+++ blocks/commerce-addresses/commerce-addresses.js
@@ -1,5 +1,6 @@
 import { Addresses } from '@dropins/storefront-account/containers/Addresses.js';
 import { render as accountRenderer } from '@dropins/storefront-account/render.js';
+import { getCustomerRolePermissions } from '@dropins/storefront-auth/api.js';
 import { getConfigValue } from '@dropins/tools/lib/aem/configs.js';
 import { readBlockConfig } from '../../scripts/aem.js';
 import {
@@ -12,6 +13,8 @@ import {
 // Initialize
 import '../../scripts/initializers/account.js';
 
+const COMPANY_ADDRESS_PERMISSION = 'Magento_CompanyAddressStorefrontCompatibility::company_address';
+
 export default async function decorate(block) {
   const isB2BEnabled = getConfigValue('commerce-b2b-enabled');
   const {
@@ -21,7 +24,16 @@ export default async function decorate(block) {
   if (!checkIsAuthenticated()) {
     window.location.href = rootLink(CUSTOMER_LOGIN_PATH);
   } else {
+    // Company address book permission decides which title the container renders.
+    const permissions = isB2BEnabled
+      ? await getCustomerRolePermissions().catch(() => ({}))
+      : {};
+    const hasCompanyAddressBook = Boolean(
+      permissions.admin || permissions[COMPANY_ADDRESS_PERMISSION],
+    );
+
     await accountRenderer.render(Addresses, {
+      title: hasCompanyAddressBook ? 'Company Addresses' : 'Addresses',
       b2bEnabled: isB2BEnabled,
       minifiedView: minifiedViewConfig === 'true',
       withActionsInMinifiedView: false,
