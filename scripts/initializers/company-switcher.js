31a061834e6f3896d8e3a390635a432d0c597809
kkozan <kkozan@adobe.com>
2026-08-25 14:28:42 -0500

    COMOPT-2093
    - Added header mounts for catalogView

--- scripts/initializers/company-switcher.js
+++ scripts/initializers/company-switcher.js
@@ -1,5 +1,6 @@
 import { initializers } from '@dropins/tools/initializer.js';
 import { initialize, setEndpoint } from '@dropins/storefront-company-switcher/api.js';
+import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import { initializeDropin } from './index.js';
 import { CORE_FETCH_GRAPHQL, CS_FETCH_GRAPHQL } from '../commerce.js';
 
@@ -11,5 +12,8 @@ await initializeDropin(async () => {
   return initializers.mountImmediately(initialize, {
     fetchGraphQlModules: [CORE_FETCH_GRAPHQL, CS_FETCH_GRAPHQL],
     groupGraphQlModules: [CS_FETCH_GRAPHQL],
+    catalogViewGraphQlModules: [CS_FETCH_GRAPHQL],
+    catalogViewHeader: 'ac-view-id',
+    catalogViewDefault: getHeaders('cs')['ac-view-id'],
   });
 })();
