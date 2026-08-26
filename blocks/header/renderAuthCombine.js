c8e5a1f84dfe892b1163cae6d7a699bd1c59bb16
Sergio Vera <svera@adobe.com>
2026-08-26 12:18:07 +0200

    Add button role to nav items for screen readers

--- blocks/header/renderAuthCombine.js
+++ blocks/header/renderAuthCombine.js
@@ -223,7 +223,7 @@ const renderAuthCombine = (navSections, toggleMenu) => {
 
     authCombineLink.classList.add('authCombineNavElement');
     const text = authCombineLink.textContent || '';
-    authCombineLink.innerHTML = `<a href="#">${text}</a>`;
+    authCombineLink.innerHTML = `<button type="button">${text}</button>`;
     authCombineLink.addEventListener('click', (event) => {
       event.preventDefault();
       onHeaderLinkClick(accountLi);
