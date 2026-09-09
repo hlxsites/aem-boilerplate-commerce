f4518c273307ef6396105a8a67442b87e9508e03
Miquel Angel Coca Piza <cocapiza@Miquels-MacBook-Pro.local>
2026-09-09 12:12:53 +0200

    Fixed lint issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,7 +9,7 @@ import {
   loadSections,
   loadCSS,
   buildBlock,
-} from "./aem.js";
+} from './aem.js';
 import {
   loadCommerceEager,
   loadCommerceLazy,
@@ -20,7 +20,7 @@ import {
   decorateSections,
   IS_UE,
   IS_DA,
-} from "./commerce.js";
+} from './commerce.js';
 
 /*
  * Trusted Types default policy.
@@ -52,32 +52,32 @@ import {
  * - Trusted Types API: https://developer.mozilla.org/en-US/docs/Web/API/Trusted_Types_API
  */
 if (window.trustedTypes && window.trustedTypes.createPolicy) {
-  const innerTT = window.trustedTypes.createPolicy("tt-inner", {
+  const innerTT = window.trustedTypes.createPolicy('tt-inner', {
     createHTML: (s) => s, // avoid stack overflow
   });
 
-  window.trustedTypes.createPolicy("default", {
+  window.trustedTypes.createPolicy('default', {
     createHTML: (input, type, sink) => {
       let processedInput = input;
       if (/srcdoc\s*=/i.test(processedInput)) {
         const doc = new DOMParser().parseFromString(
           innerTT.createHTML(processedInput),
-          "text/html",
+          'text/html',
         );
         doc
-          .querySelectorAll("iframe[srcdoc]")
-          .forEach((el) => el.removeAttribute("srcdoc"));
+          .querySelectorAll('iframe[srcdoc]')
+          .forEach((el) => el.removeAttribute('srcdoc'));
         processedInput = doc.body.innerHTML;
       }
       if (
-        sink.includes("createContextualFragment") ||
-        sink.includes("Document write")
+        sink.includes('createContextualFragment')
+        || sink.includes('Document write')
       ) {
         const doc = new DOMParser().parseFromString(
           innerTT.createHTML(processedInput),
-          "text/html",
+          'text/html',
         );
-        doc.querySelectorAll("script").forEach((el) => el.remove());
+        doc.querySelectorAll('script').forEach((el) => el.remove());
         processedInput = doc.body.innerHTML;
       }
       return processedInput;
@@ -93,8 +93,7 @@ if (window.trustedTypes && window.trustedTypes.createPolicy) {
 async function loadFonts() {
   await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
   try {
-    if (!window.location.hostname.includes("localhost"))
-      sessionStorage.setItem("fonts-loaded", "true");
+    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
   } catch (e) {
     // do nothing
   }
@@ -107,15 +106,15 @@ async function loadFonts() {
 function buildWidgetAutoBlocks(main) {
   const widgetLinks = [...main.querySelectorAll('a[href*="/widgets/"]')];
   widgetLinks.forEach((link) => {
-    if (link.closest(".widget")) return;
+    if (link.closest('.widget')) return;
     const newLink = link.cloneNode(true);
-    const widgetBlock = buildBlock("widget", { elems: [newLink] });
-    const p = link.closest("p");
+    const widgetBlock = buildBlock('widget', { elems: [newLink] });
+    const p = link.closest('p');
     if (
-      p &&
-      p.querySelectorAll("a").length === 1 &&
-      p.querySelector("a") === link &&
-      p.textContent.trim() === link.textContent.trim()
+      p
+      && p.querySelectorAll('a').length === 1
+      && p.querySelector('a') === link
+      && p.textContent.trim() === link.textContent.trim()
     ) {
       p.replaceWith(widgetBlock);
     } else {
@@ -133,10 +132,10 @@ function buildAutoBlocks(main) {
     // auto load `*/fragments/*` references
     const fragments = [
       ...main.querySelectorAll('a[href*="/fragments/"]'),
-    ].filter((f) => !f.closest(".fragment"));
+    ].filter((f) => !f.closest('.fragment'));
     if (fragments.length > 0) {
       // eslint-disable-next-line import/no-cycle
-      import("../blocks/fragment/fragment.js").then(({ loadFragment }) => {
+      import('../blocks/fragment/fragment.js').then(({ loadFragment }) => {
         fragments.forEach(async (fragment) => {
           try {
             const { pathname } = new URL(fragment.href);
@@ -144,14 +143,14 @@ function buildAutoBlocks(main) {
             fragment.parentElement.replaceWith(...frag.children);
           } catch (error) {
             // eslint-disable-next-line no-console
-            console.error("Fragment loading failed", error);
+            console.error('Fragment loading failed', error);
           }
         });
       });
     }
     buildWidgetAutoBlocks(main);
   } catch (error) {
-    console.error("Auto Blocking failed", error);
+    console.error('Auto Blocking failed', error);
   }
 }
 
@@ -160,13 +159,13 @@ function buildAutoBlocks(main) {
  * @param {HTMLElement} main The main container element
  */
 function decorateButtons(main) {
-  main.querySelectorAll("p a[href]").forEach((a) => {
+  main.querySelectorAll('p a[href]').forEach((a) => {
     a.title = a.title || a.textContent;
-    const p = a.closest("p");
+    const p = a.closest('p');
     const text = a.textContent.trim();
 
     // quick structural checks
-    if (a.querySelector("img") || p.textContent.trim() !== text) return;
+    if (a.querySelector('img') || p.textContent.trim() !== text) return;
 
     // skip URL display links
     try {
@@ -176,22 +175,22 @@ function decorateButtons(main) {
     }
 
     // require authored formatting for buttonization
-    const strong = a.closest("strong");
-    const em = a.closest("em");
+    const strong = a.closest('strong');
+    const em = a.closest('em');
     if (!strong && !em) return;
 
-    p.className = "button-wrapper";
-    a.className = "button";
+    p.className = 'button-wrapper';
+    a.className = 'button';
     if (strong && em) {
       // high-impact call-to-action
-      a.classList.add("accent");
+      a.classList.add('accent');
       const outer = strong.contains(em) ? strong : em;
       outer.replaceWith(a);
     } else if (strong) {
-      a.classList.add("primary");
+      a.classList.add('primary');
       strong.replaceWith(a);
     } else {
-      a.classList.add("secondary");
+      a.classList.add('secondary');
       em.replaceWith(a);
     }
   });
@@ -215,10 +214,10 @@ export function decorateMain(main) {
  * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  document.documentElement.lang = "en";
+  document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
 
-  const main = doc.querySelector("main");
+  const main = doc.querySelector('main');
   if (main) {
     try {
       await initializeCommerce();
@@ -226,16 +225,16 @@ async function loadEager(doc) {
       applyTemplates(doc);
       await loadCommerceEager();
     } catch (e) {
-      console.error("Error initializing commerce configuration:", e);
+      console.error('Error initializing commerce configuration:', e);
       loadErrorPage(418);
     }
-    document.body.classList.add("appear");
-    await loadSection(main.querySelector(".section"), waitForFirstImage);
+    document.body.classList.add('appear');
+    await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
 
   try {
     /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
-    if (window.innerWidth >= 900 || sessionStorage.getItem("fonts-loaded")) {
+    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
       loadFonts();
     }
   } catch (e) {
@@ -248,16 +247,16 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  loadHeader(doc.querySelector("body > header"));
+  loadHeader(doc.querySelector('body > header'));
 
-  const main = doc.querySelector("main");
+  const main = doc.querySelector('main');
   await loadSections(main);
 
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadFooter(doc.querySelector("body > footer"));
+  loadFooter(doc.querySelector('body > footer'));
 
   loadCommerceLazy();
 
@@ -270,7 +269,7 @@ async function loadLazy(doc) {
  * without impacting the user experience.
  */
 function loadDelayed() {
-  import("./consent-check.js");
+  import('./consent-check.js');
   // load anything that can be postponed to the latest here
 }
 
@@ -293,7 +292,7 @@ loadPage();
 (async function loadDa() {
   if (!IS_DA) return;
   // eslint-disable-next-line import/no-unresolved
-  import("https://da.live/scripts/dapreview.js").then(
+  import('https://da.live/scripts/dapreview.js').then(
     ({ default: daPreview }) => daPreview(loadPage),
   );
-})();
+}());
