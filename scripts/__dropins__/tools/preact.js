1200d928b71cb2079c1b747b573d6ff7c551919f
Jaume Capdevila <capdevil@adobe.com>
2026-09-16 16:37:29 +0200

    Revert "try pinning preact to v10.22.1"
    This reverts commit 246cb2394040974bb78d6759ebefc9d2a30dfb2b.

--- scripts/__dropins__/tools/preact.js
+++ scripts/__dropins__/tools/preact.js
@@ -1,17 +1,4 @@
-/*! TEMPORARY TEST OVERRIDE: re-exports from a vendored Preact build (see
-scripts/vendor/preact/) instead of the version bundled with @dropins/tools,
-for testing purposes only. Revert to the original @dropins/tools build to
-restore the pinned version. */
-export {
-  Component,
-  Fragment,
-  cloneElement,
-  createContext,
-  createElement,
-  createElement as h,
-  hydrate,
-  isValidElement,
-  options,
-  render,
-  toChildArray,
-} from 'preact';
+/*! Copyright 2026 Adobe
+All Rights Reserved. */
+import{C as t,S as s,W as r,X as n,k as o,M as l,k as m,U as c,t as d,l as i,R as C,F as h}from"./chunks/preact-vendor.js";export{t as Component,s as Fragment,r as cloneElement,n as createContext,o as createElement,l as createRef,m as h,c as hydrate,d as isValidElement,i as options,C as render,h as toChildArray};
+//# sourceMappingURL=preact.js.map
